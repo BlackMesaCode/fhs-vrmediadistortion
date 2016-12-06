075b8e2b0148ce4366176096dfaf19275ce13d3b
Lukas Klappert <lklappert.mmt-b2013@fh-salzburg.ac.at>
2016-12-06 10:33:04 +0100

    Version with 2 walls

--- unity-project/VR Media Distortion/Assets/Scripts/ImageDeactivator.cs
+++ unity-project/VR Media Distortion/Assets/Scripts/ImageDeactivator.cs
@@ -8,12 +8,32 @@ public class ImageDeactivator : MonoBehaviour {
     public float minTimeUntilActivation = 3f;
     public float maxTimeUntilActivation = 10f;
 
+    public float minTimeImageChange = 1f;
+    public float maxTimeImageChange = 5f;
+
+    public int maxLives = 3;
+    private int curLives;
+
     public bool activated = true;
+
+    public ImageManager imageManager;
 
     // Use this for initialization
     void Start () {
-		
+        curLives = maxLives;
+        imageManager = GameObject.FindWithTag("ImageManager").GetComponent<ImageManager>();
+
+        InvokeRepeating("ChangeImage", 0f, Random.Range(minTimeImageChange, maxTimeImageChange));
 	}
+
+    private void ChangeImage()
+    {
+        if (activated)
+        {
+            Material mat = imageManager.GetRandomMaterial();
+            GetComponent<Renderer>().material = mat;
+        }
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -25,23 +45,27 @@ public class ImageDeactivator : MonoBehaviour {
         if (activated)
         {
             activated = false;
+            --curLives;
             gameObject.GetComponent<Renderer>().material.color = Color.black;
-            gameObject.GetComponent<Light>().enabled = false;
+            gameObject.GetComponentInChildren<Light>().enabled = false;
 
             if (Random.Range(0f, 1f) < randomValueThresholdActivateAgain)
             {
                 Invoke("Activate", Random.Range(minTimeUntilActivation, maxTimeUntilActivation));
+            } else
+            {
+                curLives = 0;
             }
         }
     }
 
     private void Activate()
     {
-        if (!activated)
+        if (!activated && curLives > 0)
         {
             activated = true;
             gameObject.GetComponent<Renderer>().material.color = Color.white;
-            gameObject.GetComponent<Light>().enabled = true;
+            gameObject.GetComponentInChildren<Light>().enabled = true;
         }
     }
 }
--- unity-project/VR Media Distortion/Assets/Scripts/ImageManager.cs
+++ unity-project/VR Media Distortion/Assets/Scripts/ImageManager.cs
@@ -1,16 +1,78 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class ImageManager : MonoBehaviour {
 
+    public string imageFolder = @".\Assets\Images";
+    private List<Material> materials;
+
 	// Use this for initialization
 	void Start () {
-		
+
 	}
 	
+    void Awake()
+    {
+        materials = GetMaterials(); 
+    }
+
+    private List<Material> GetMaterials()
+    {
+        var materials = new List<Material>();
+        var jpgFiles = Directory.GetFiles(imageFolder, "*.jpg");
+        var pngFiles = Directory.GetFiles(imageFolder, "*.png");
+
+        var files = new string[jpgFiles.Length + pngFiles.Length];
+        jpgFiles.CopyTo(files, 0);
+        pngFiles.CopyTo(files, jpgFiles.Length);
+
+        foreach (var file in files)
+        {
+            Texture2D tex = LoadImage(file);
+            Material mat = CreateMat(tex);
+            materials.Add(mat);
+        }
+        return materials;
+    }
+
+
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    public Material GetRandomMaterial()
+    {
+        var randomIndex = Random.Range(0, materials.Count - 1);
+        return materials[randomIndex];
+    }
+
+    private static Texture2D LoadImage(string filePath)
+    {
+        Texture2D tex = null;
+        byte[] fileData;
+
+        if (File.Exists(filePath))
+        {
+            fileData = File.ReadAllBytes(filePath);
+            if (fileData == null || fileData.Length == 0)
+                Debug.Log($"{filePath} is null");
+            tex = new Texture2D(2, 2);
+            if (!tex.LoadImage(fileData))
+                Debug.Log("Load image failed for: " + filePath);
+        }
+        else
+            Debug.Log("missing: " + filePath);
+        return tex;
+    }
+
+    private Material CreateMat(Texture2D texture)
+    {
+        Material mat = new Material(Shader.Find("Standard"));
+        mat.SetTexture("_MainTex", texture);
+        mat.color = Color.white;
+        return mat;
+    }
 }
