ab809090ae886fb59e2a44663828738340971b39
Lukas Klappert <lklappert.mmt-b2013@fh-salzburg.ac.at>
2016-12-06 12:34:15 +0100

    version with 4 walls

---
+++ unity-project/VR Media Distortion/Assets/Scripts/GameManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    private List<ImageDeactivator> images;
+    public Light ambientLight;
+    public GameObject floor;
+    public WallMover[] walls;
+    public Material grass;
+
+	// Use this for initialization
+	void Start () {
+        GameObject[] imageObjects = GameObject.FindGameObjectsWithTag("Image");
+        images = new List<ImageDeactivator>();
+        foreach (GameObject obj in imageObjects)
+        {
+            images.Add(obj.GetComponent<ImageDeactivator>());
+        }
+        ambientLight.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (GameWon())
+        {
+            foreach (WallMover wall in walls)
+            {
+                wall.StartWinningSequence();
+            }
+            ambientLight.enabled = true;
+            floor.GetComponent<Renderer>().material = grass;
+        }
+	}
+
+    private bool GameWon()
+    {
+        foreach (ImageDeactivator img in images)
+        {
+            if (img.activated)
+                return false;
+        }
+        return true;
+    }
+}
--- unity-project/VR Media Distortion/Assets/Scripts/WallMover.cs
+++ unity-project/VR Media Distortion/Assets/Scripts/WallMover.cs
@@ -4,17 +4,50 @@ using UnityEngine;
 
 public class WallMover : MonoBehaviour {
     public float speed = 1f;
+    public float winningSeqSpeed = 2f;
+    public float winningSeqAcceleration = 1.1f;
     public Transform target;
+
+    private bool isInWinningSequence = false;
+    public float winningSequenceYPosition = 5f;
+
+    private float winningSeqCurSpeed;
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+        winningSeqCurSpeed = winningSeqSpeed;
+    }
 	
 	// Update is called once per frame
 	void Update () {
+        if (!isInWinningSequence)
+        {
+            MoveToTarget();
+        } else
+        {
+            WinningSequence();
+        }
+    }
+
+    private void MoveToTarget()
+    {
         float step = speed * Time.deltaTime;
-        Vector3 newPosition = new Vector3(transform.position.x, transform.position.y, Vector3.MoveTowards(transform.position, target.position, step).z);
+        //Vector3 newPosition = new Vector3(transform.position.x, transform.position.y, Vector3.MoveTowards(transform.position, target.position, step).z);
+        Vector3 newPosition = Vector3.MoveTowards(transform.position, target.position, step);
+        transform.position = newPosition;
+    }
+
+    public void StartWinningSequence()
+    {
+        isInWinningSequence = true;
+    }
+
+    private void WinningSequence()
+    {
+        winningSeqCurSpeed *= 1 + winningSeqAcceleration * Time.deltaTime;
+        float step = winningSeqCurSpeed * Time.deltaTime;
+        Vector3 newTarget = new Vector3(transform.position.x, winningSequenceYPosition, transform.position.z);
+        Vector3 newPosition = Vector3.MoveTowards(transform.position, newTarget, step);
         transform.position = newPosition;
     }
 }
